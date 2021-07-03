9ed4b43eb102d250303c6591095b09a556cddd84
actuanic <62603444+actuanic@users.noreply.github.com>
2021-07-03 21:53:00 +0700

    check

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Counters from "./components/counter";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function App() {
+  return (
+    <div className="App">
+    
+      <Counters />
+    </div>
+  );
+}
